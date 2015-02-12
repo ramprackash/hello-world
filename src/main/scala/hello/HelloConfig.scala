e117f351beae2d4fbbba8d4032719389323017ee
Ubuntu <ubuntu@ip-172-31-40-147.us-west-2.compute.internal>
2015-02-12 07:30:18 +0000

    GET and POST skeletons explicitly instead of @RestController

--- src/main/scala/hello/HelloConfig.scala
+++ src/main/scala/hello/HelloConfig.scala
@@ -2,7 +2,11 @@ package hello
 
 import org.springframework.context.annotation.{Configuration, ComponentScan}
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.web.bind.annotation.{RequestMapping, ResponseBody, RequestParam, RestController}
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
 
 /**
  * This config class will trigger Spring @annotation scanning and auto configure Spring context.
@@ -15,10 +19,12 @@ import org.springframework.web.bind.annotation.{RequestMapping, ResponseBody, Re
 @ComponentScan
 @RestController
 class HelloConfig {
-	@RequestMapping(Array("/"))
-	def home() = {
-		"Hello world - from Scala!";
-	}
-
+	@RequestMapping(value=Array("/"), method=Array(RequestMethod.GET))
+        def home() = {
+        "Hello world!";
+    }
+	@RequestMapping(value=Array("/"), method=Array(RequestMethod.POST))
+        def homepost() = {
+        "Post world!";
+    }
 }
-
